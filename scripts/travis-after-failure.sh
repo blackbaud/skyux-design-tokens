292704391106f7852c88eb84fbe136c6abf43ce8
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-06-23 10:31:50 -0400

    Implement GitHub Actions

--- scripts/travis-after-failure.sh
+++
@@ -1,7 +0,0 @@
-# Fail the build if this step fails
-set -e
-
-# Display any npm errors
-if [ -e ./npm-debug.log ]; then
-  cat ./npm-debug.log
-fi
--- scripts/travis-after-success.sh
+++
@@ -1,7 +0,0 @@
-# Fail the build if this step fails
-set -e
-
-if [[ "$TRAVIS_SECURE_ENV_VARS" == "true" && -n "$TRAVIS_TAG" ]]; then
-  npm run clean
-  npm run build
-fi
