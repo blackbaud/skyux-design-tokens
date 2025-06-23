7cf5c107b3f0a5b38946268da9fdd82f23e46c88
Matt Gregg <matt.gregg@blackbaud.com>
2025-06-23 17:15:03 -0400

    new blue 25 page background

--- src/dev/dev-main.ts
+++ src/dev/dev-main.ts
@@ -1,8 +1,1 @@
 /* Add any logic for local development to this file. `main.ts` should remain empty for build purposes. */
-document.body.classList.add(
-  'local-dev-tokens',
-  'sky-theme-modern',
-  'sky-theme-brand-base',
-);
-
-document.body.classList.remove('sky-theme-default');
